f01b7a00c40acb1612c78dea776e27bbd0518a3c
Carlos E. Carvalho <carlosalvc@gmail.com>
2015-11-17 00:17:42 +0000

    Added migrations v1

---
+++ Shuttle/database/migrations/2015_11_17_000429_create_shuttle_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShuttleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shuttles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('seats');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('shuttles');
+    }
+}
--- Shuttle/database/migrations/2015_11_17_000458_create_roles_table.php
+++ Shuttle/database/migrations/2015_11_17_000458_create_roles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +12,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('username')->unique();
-            $table->string('password', 60);
-            $table->string('id_document')->unique();
-            $table->integer('karma');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('roles');
     }
 }
